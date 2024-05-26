4cceaf824aa3bd97cefa984709c91d6bb7fd96cf
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-26 18:35:21 -0500

    feat: DefaultConnection link to sql and setup KebabCaseRouteNamingConvention

--- AgriSage.API/Program.cs
+++ AgriSage.API/Program.cs
@@ -1,6 +1,14 @@
+using AgriSage.API.Shared.Interfaces.ASP.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers( options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
