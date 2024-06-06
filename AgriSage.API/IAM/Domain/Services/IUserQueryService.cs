e8585e1970aa38a4930c648ab92089625eef15ff
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-06-06 18:05:04 -0500

    feat(iam): added the domain layer with the default user Repository

---
+++ AgriSage.API/IAM/Domain/Services/IUserQueryService.cs
@@ -0,0 +1,11 @@
+﻿using AgriSage.API.IAM.Domain.Model.Aggregates;
+using AgriSage.API.IAM.Domain.Model.Commands;
+
+namespace AgriSage.API.IAM.Domain.Services;
+
+public interface IUserCommandService
+{
+    Task<(User user, string token)> Handle(SignInCommand command);
+    
+    Task Handle(SignUpCommand command);
+}
--- AgriSage.API/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
+++ AgriSage.API/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -1,5 +1,5 @@
 ﻿using AgriSage.API.IAM.Domain.Model.Aggregates;
-using AgriSage.API.IAM.Domain.Repositories;
+using AgriSage.API.IAM.IAM.Domain.Repositories;
 using AgriSage.API.IAM.Shared.Infrastructure.Persistence.EFC.Configuration;
 using AgriSage.API.IAM.Shared.Infrastructure.Persistence.EFC.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -8,25 +8,13 @@ namespace AgriSage.API.IAM.Infrastructure.Persistence.EFC.Repositories;
 
 public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository
 {
-    /**
-     * <summary>
-     *     Find a user by username
-     * </summary>
-     * <param name="username">The username to search</param>
-     * <returns>The user</returns>
-     */
+
     public async Task<User?> FindByUsernameAsync(string username)
     {
         return await Context.Set<User>().FirstOrDefaultAsync(user => user.Username.Equals(username));
     }
 
-    /**
-     * <summary>
-     *     Check if a user exists by username
-     * </summary>
-     * <param name="username">The username to search</param>
-     * <returns>True if the user exists, false otherwise</returns>
-     */
+
     public bool ExistsByUsername(string username)
     {
         return Context.Set<User>().Any(user => user.Username.Equals(username));
